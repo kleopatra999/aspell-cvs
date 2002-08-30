7ada4f1b81e63547380f1904f951c58b0af83014
Kevin Atkinson <kevina@gnu.org>
2002-08-30 20:02:14 +0000

    Fixed a bug in which the last line without newline will be ignored.
    [Imported from GNU Aspell CVS Repository.]

--- prog/aspell.cpp
+++ prog/aspell.cpp
@@ -492,7 +492,6 @@ void pipe()
     while (c = getchar(), c != '\n' && c != EOF)
       buf.push_back(static_cast<char>(c));
     buf.push_back('\0');
-    if (c == EOF) break;
     line = buf.data();
     ignore = 0;
     switch (line[0]) {
@@ -636,6 +635,7 @@ void pipe()
       }
       COUT << "\n";
     }
+    if (c == EOF) break;
   }
 
   delete_aspell_speller(speller);
