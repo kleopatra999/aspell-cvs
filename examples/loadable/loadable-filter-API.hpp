8a3e7a8a8a0f4b8b78c1fc3b1040774a1ad7c2d2
Christoph Hintermüller <nothere@nongnu.org>
2003-02-06 22:30:33 +0000

    The heavy changes to the programmers interface respective to the macros for defining the filter description and the corresponding options and their description, default values, type ... are necessary for making the existing builtin filters loadable too. The filters which will become loadable are the TeX/LaTeX, the SGML and the HTML filter. The url filter will stay static as it is needed nearly in every case. The API now consist of several macros which provide a maximum of readbility of the option definitions. Further it is now possible to use the same macro more than once within the source files to retrieve the settings more than once. This is mostly usefull for filters providing different classes for encoder, filter and decoder feature each equiped with a setup member function.
    A filter defintion consists at least of the following steps
    
    1) declare a class containing the filter which is derived from IndividualFilter class
    2) use FILTER, ENCODER, DECODER macro to denote the class as decoder, encoder or filter featrue
    3) Use SET_DESCRIPTION macro with name set equal to filter to provide a short description of what the filter does.
    4) use ACTIVATE_ENCODER, ACTIVATE_FILTER, ACTIVATE_DECODER macro to make the corresponding filter feature callable by aspell and to tell ml-flt-opt.pl that the corresponding options should be added to the .opt file of the filter. All options occuring only within a non activated filter feature will be ignored.
    5) use BOOL_OPTION, INT_OPTION, STRING_OPTION or/and LIST_OPTION macro to define the entire options.
    6) use SET_DESCRIPTION macro to denote helptext for option
    7) use SET_DEFAULT or ADD_DEFAULT alternatively for list type options to set defaultvalues.
    8) repeat 5) to 7) for all options belonging to a filter featrue
    9) repeat 1) to 8) for all desired features
    
    Note: step 3) is only required once per filter, if it occures more than once the last version is chosen. The same applies to steps 6) and 7) except for list type options when ADD_DEFAULT macro is used, in this case all default values ever defined will be used.
    
    [Imported from GNU Aspell CVS Repository.]

--- examples/loadable/loadable-filter-API.hpp
+++ examples/loadable/loadable-filter-API.hpp
@@ -16,6 +16,7 @@
 #define LOADABLE_FILTER_API_HEADER
 #include "posib_err.hpp"
 #include "config.hpp"
+#include "indiv_filter.hpp"
 
 #if defined(__CYGWIN__) || defined (_WIN32)
 #define FILTER_API_EXPORTS __declspec(dllexport)
@@ -25,74 +26,177 @@
 #define FILTER_API_IMPORTS
 #endif
 
-//do not call directly as called by OPTION and NEW_OPTION macro
-#ifndef string_opt
-#define string_opt(config,name,returns,filter) \
-        RET_ON_ERR_SET(config->retrieve("filter-"filter"-"name),String,returns)
+/* The following macros hide some details about option retrievement in aspell.
+ * They offer a common interface to the different types of options:
+ * Bool:   Option of boolean type;
+ * Int:    Number  value;
+ * String: Option containing a std::String;
+ * List:   returns a list of strings; use StringEnumeration from aspell 
+ *         common directory.
+ *
+ * For each of these macros the following parameters have to be specified:
+ * config:  the name of the configuration object.
+ * name:    a string containing name of the option to retrieve.
+ * returns: the name of the variable the value of the option shoul be saved
+ *          to, in case of list this has to be maintained by filter.
+ * filter:  as there might be nore than one filters contained in one source
+ *          file it is necessary to specifiy the name of the filter the
+ *          option belongs to.
+ */
+#ifndef STRING_OPTION
+#define STRING_OPTION(config,name,returns,filter,feature) \
+        RET_ON_ERR_SET(config->retrieve("filter-"filter"-"name),String,returns)}
 #endif
 
-//do not call directly as called by OPTION and NEW_OPTION macro
-#ifndef bool_opt
-#define bool_opt(config,name,returns,filter) \
+#ifndef BOOL_OPTION
+#define BOOL_OPTION(config,name,returns,filter,feature) \
         RET_ON_ERR_SET(config->retrieve_bool("filter-"filter"-"name),bool,returns)
 #endif
 
-//do not call directly as called by OPTION and NEW_OPTION macro
-#ifndef int_opt
-#define int_opt(config,name,returns,filter) \
+#ifndef INT_OPTION
+#define INT_OPTION(config,name,returns,filter,feature) \
         RET_ON_ERR_SET(config->retrieve_int("filter-"filter"-"name),int,returns)
 #endif
 
-//do not call directly as called by OPTION and NEW_OPTION macro
-#ifndef list_opt
-#define list_opt(config,name,returns,filter) \
+#ifndef LIST_OPTION
+#define LIST_OPTION(config,name,returns,filter,feature) \
         RET_ON_ERR(config->retrieve_list("filter-"filter"-"name,returns))
 #endif
 
-//use this if you have already called NEW_OPTION once for a specific
-//option to retrieve it 
-#ifndef OPTION
-#define OPTION(name,type,config,returns,filter) \
-        type##_opt(config,name,returns,filter)
+/* The following macros have no impact on the behaviour of the filter, but
+ * they allow the mk-flt-opt.pl script to compile the .opt file from the
+ * sources. Even if the mk-flt-opt.pl script is not used during developement
+ * of filter they should be specified inside the sources, as this eases
+ * inclusion of the filter into Aspell standard distribution.
+ */
+
+/* ENCODER, FILTER, DECODER:
+ *          Indicates that the following part of Code belongs to a new iencoder,
+ *          filter oder decoder. The name of the filter is specified by the
+ *          string name, which is stored in the variable save, it can be called
+ *          from any point of your code. 
+ * Note: Call this macro inside your filter class declaration.
+ */  
+
+#ifndef ENCODER
+#define ENCODER(name,save,class_name) \
+public: const char * save(void) { \
+    return name;\
+  }
 #endif
 
-//!NEW_OPTION also used by make_opt to extract *.opt file
-//!name: the name of the option
-//!type: the type of the option
-//!config: the configuration from which the option should be
-//!        retrieved
-//!returns: the variable the contents of the options should
-//!         be saved to
-//!def: the default value of the option
-//!des: the help text
-//!filter: the name of the filter the option belongs to
-//!        see DESCRIPTION macro below
-//!        it is required if .opt file is generated automatically
-//!use `\;' instead of `,' in def and des field 
-//!the `\;' will be expanded by make_opt.awk to `,'
-//!if you use `,' directly in des and def  make_opt.awk will be
-//!confused and abort with an improper option error. 
-#ifndef NEW_OPTION
-#define NEW_OPTION(name,type,config,returns,def,des,filter) \
-        OPTION(name,type,config,returns,filter)
+#ifndef FILTER
+#define FILTER(name,save,class_name) \
+public: const char * save(void) { \
+    return name;\
+  }
 #endif
 
-//!used by make_opt to extract *.opt file
-//!name is the name of the filter version
-//!ver is the actual version of the filter
-//!desc a short description of the filter
-//!aspell is the version of Aspell required by filter
-//!       it may start with >= <= =,
-//!       per default = will be assumed
-//!save static variable the name will be saved to.
-//!it may be used to initialise the name member of IndividualFilter
-//!use `\;' instead of `,' in the desc field
-//!the `\;' will be expanded by make_opt.awk to `,'
-//!if you use `,' directly in desc field make_opt.awk will be
-//!confused and abort with an improper filter error. 
-#ifndef DESCRIPTION
-#define DESCRIPTION(name,ver,desc,aspell,save) \
-        static char* save = name;
+#ifndef DECODER
+#define DECODER(name,save,class_name) \
+public: const char * save(void) { \
+    return name;\
+  }
+#endif
+
+/* ACTIVATE_ENCODER, ACTIVATE_FILTER, ACTIVATE_DECODER:
+ *          Before a encoding, decoding or processing filter can be
+ *          loaded by aspell filter interface it has to be activated.
+ *          If a specific class is denoted as encoder, decoder or filter
+ *          by one of the above macros use the following ones to activate
+ *          the entire filter feature.
+ *          If not than write a wraperfunction named new_fencoder, new_filter or
+ *          new_decoder and declare ist as extern C. This function should look like
+ *          
+ *          extern "C" acommon::IndividualFilter * new_<feture>(void){
+ *            return new <YourFilterClass>;
+ *          }
+ *
+ *          <feature> stands for either encoder, filter or decoder;
+ *          <YourFilterClass> is the name of the corresponding class.
+ * Note: call this outside your filter class declaration. If your filter class
+ *       does not contain to any specific namespace or these macros are called
+ *       inside the same namespace as the class containing the entire feature, 
+ *       nspace may be empty (not tested);
+ */
+#ifndef ACTIVATE_ENCODER
+#define ACTIVATE_ENCODER(nspace,class_name,filter) \
+extern "C" { \
+  acommon::IndividualFilter * new_encoder(void) {\
+    return new nspace::class_name;\
+  }\
+}
+#endif
+#ifndef ACTIVATE_FILTER
+#define ACTIVATE_FILTER(nspace,class_name,filter) \
+extern "C" { \
+  acommon::IndividualFilter * new_filter(void) {\
+    return new nspace::class_name;\
+  }\
+}
+#endif
+#ifndef ACTIVATE_DECODER
+#define ACTIVATE_DECODER(nspace,class_name,filter) \
+  acommon::IndividualFilter * new_decoder(void) {\
+    return new nspace::class_name;\
+  }\
+extern "C" { \
+}
+#endif
+
+/* ASPELL_VERSION: This may be used to explicitly specify the Aspell version
+ *                 the filter should be usable for. The version number may 
+ *                 be prefixed by `<', `<=', `=', `>=' or `>' to exclude
+ *                 some versions of aspell and admit the consecutive or prior
+ *                 versions. ASPELL_VERSION is not exolicitly specified
+ *                 mk-flt-opt.pl assumes the actual version prefixed by `>='.
+ */
+#ifndef ASPELL_VERSION
+#define ASPELL_VERSION(name,version)
+#endif
+
+/* SET_DESCRIPTION: This should be used to specify a short description of the
+ *                  Filter. If mk-flt-opt.pl is used to generate the .opt
+ *                  file Aspell will display this line as a short description.
+ *                  of the filter or filter option when running help and
+ *                  config action.
+ *
+ *                  If "name" is equal to "filter" and a filter named "name"
+ *                  exists mk-flt-opt.pl will use descript to generate the
+ *                  description of the filter. In any other case
+ *                  mk-flt-opt.pl tries to generate the description, for the
+ *                  option "name" for filter "filter".
+ *                  
+ * Note: If there is need for one of the following chars use their synonym as 
+ *       shown below. This is necessary to keep mk-flt-opt.pl as simple as
+ *       possible.
+ *       Use `\{' for `(' and `\}' for `)';
+ *       Use `\;' for `,' and `\'' for `"'; 
+ */
+#ifndef SET_DESCRIPTION
+#define SET_DESCRIPTION(name,descript,filter)
+#endif
+
+/* SET/ADD_DEFAULT: Sets or adds the string "set" to the default values for
+ *                  the option "name" of filter "filter".
+ *                  
+ * Note: mk-flt-opt.pl will accept default values set via SET_DEFAULT macro
+ *       for all types of filter options (int,bool,string,list). ADD_DEFAULT
+ *       macro on the other hand may only be used for extending list of
+ *       default values for list type options.
+ *       
+ * Note: If there is need for one of the following chars use their synonym as 
+ *       shown below. This is necessary to keep mk-flt-opt.pl as simple as
+ *       possible.
+ *       Use `\{' for `(' and `\}' for `)';
+ *       Use `\;' for `,' and `\'' for `"'; 
+ */
+#ifndef SET_DEFAULT
+#define SET_DEFAULT(name,set,filter)
+#endif
+
+#ifndef ADD_DEFAULT
+#define ADD_DEFAULT(name,set,filter)
 #endif
 
 #endif
