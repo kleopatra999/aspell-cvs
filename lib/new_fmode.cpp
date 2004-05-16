105b1e83ef8bdebdcdf90660d25b820aa5613cdc
Christoph Hintermüller <nothere@nongnu.org>
2004-05-16 19:52:56 +0000

    fixed vector backward iterator problem in new_fmode; ensure committing all changes done on aspell on fmodes.
    [Imported from GNU Aspell CVS Repository.]

--- lib/new_fmode.cpp
+++ lib/new_fmode.cpp
@@ -96,56 +96,70 @@ using namespace std;//needed for vector
   }
 
   PosibErr<bool> FilterMode::addModeExtension(const String & ext, String toMagic) {
+fprintf(stderr,"mag: \"%s\" ext \"%s\"\n",toMagic.c_str(),ext.c_str());
 
     bool extOnly = false;
     
+DEBUG
     if (    ( toMagic == "" )
          || ( toMagic == "<nomagic>" )
          || ( toMagic == "<empty>" ) ) {
+DEBUG
       extOnly = true;
     }
     else {
+DEBUG
 
       PosibErr<bool> pe = FilterMode::MagicString::testMagic(NULL,toMagic,_name);
 
       if ( pe.has_err() ) {
+DEBUG
         return PosibErrBase(pe);
       }
+DEBUG
     } 
+DEBUG
 
     vector<MagicString>::iterator it;
 
     for ( it = magicKeys.begin() ; it != magicKeys.end() ; it++ ) {
+DEBUG
       if (    (    extOnly
                 && ( it->magic() == "" ) )
            || ( it->magic() == toMagic ) ) {
+DEBUG
         (*it) += ext;
         return true;
       }
+DEBUG
     }
+DEBUG
     if ( it != magicKeys.end() ) {
+DEBUG
       return false;
     }
+DEBUG
     if ( extOnly ) {
+DEBUG
       magicKeys.push_back(MagicString(_name));
-//FIXME strange things ongoing in gcc 2.95.3 
-//      or is it me knowing it only from RedHat grrr ore even SuSE 8.0 ????
-//      in other words why does reverse_iterator not cast to iterator
-//      without calling base() 
-//      it = magicKeys.rbegin();
-      it = magicKeys.rbegin().base();
     }
     else {
+DEBUG
       magicKeys.push_back(MagicString(toMagic,_name));
-//FIXME strange things ongoing in gcc 2.95.3 
-//      or is it me knowing it only from RedHat grrr ore even SuSE 8.0 ????
-//      in other words why does reverse_iterator not cast to iterator
-//      without calling base() 
-//      it = magicKeys.rend();
-      it = magicKeys.rend().base();
     }
-    (*it) += ext;
-    return true;
+    for ( it = magicKeys.begin() ; it != magicKeys.end() ; it++ ) {
+DEBUG
+      if (    (    extOnly
+                && ( it->magic() == "" ) )
+           || ( it->magic() == toMagic ) ) {
+DEBUG
+        (*it) += ext;
+        return true;
+      }
+DEBUG
+    }
+DEBUG
+    return make_err(mode_extend_expand,_name.c_str());
   }
 
   PosibErr<bool> FilterMode::remModeExtension(const String & ext, String toMagic) {
@@ -1052,12 +1066,14 @@ fine_next_line:
 
             PosibErr<bool> pe;
 
+fprintf(stderr,"extension \"%s\"\n",ext.c_str());
             if ( remove ) { 
               pe = collect.remModeExtension(ext,magic);
             }
             else {
               pe = collect.addModeExtension(ext,magic);
             }
+fprintf(stderr,"extension \"%s\"\n",ext.c_str());
             if ( pe.has_err() ) {
               fclose(in);
               regfree(&seekfor);
