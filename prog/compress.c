01354f42c80c5fe0e10ba67336044ab4e5560837
Kevin Atkinson <kevina@gnu.org>
2004-06-27 05:14:05 +0000

    sync with head
    [Imported from GNU Aspell CVS Repository.]

--- prog/compress.c
+++ prog/compress.c
@@ -28,24 +28,28 @@
 
 #endif
 
+#define BUFSIZE 256
+
 void usage () 
 {
   fputs("Compresses or uncompresses sorted word lists.\n"     , stderr);
   fputs("For best result the locale should be set to C\n"    , stderr);
   fputs("before sorting by setting the environmental\n"       , stderr);
   fputs("variable LANG to \"C\" before sorting.\n"            , stderr);
-  fputs("Copyright 2001 by Kevin Atkinson.\n"  , stderr);
+  fputs("Copyright 2001,2004 by Kevin Atkinson.\n"  , stderr);
   fputs("Usage: word-list-compress c[ompress]|d[ecompress]\n" , stderr);
 }
 
-static int get_word(FILE * in, char * w) 
+// PRECOND: bufsize >= 2
+static int get_word(FILE * in, char * w, size_t bufsize) 
 {
   int c;
   while (c = getc(in), c != EOF && c <= 32);
   if (c == EOF) return 0;
   do {
     *w++ = (char)(c);
-  } while (c = getc(in), c != EOF && c > 32);
+    --bufsize;
+  } while (c = getc(in), c != EOF && c > 32 && bufsize > 1);
   *w = '\0';
   ungetc(c, in);
   if (c == EOF) return 0;
@@ -61,28 +65,28 @@ int main (int argc, const char *argv[]) {
     
   } else if (argv[1][0] == 'c') {
 
-    char s1[256];
-    char s2[256];
+    char s1[BUFSIZE];
+    char s2[BUFSIZE];
     char * prev = s2;
     char * cur = s1;
     *prev = '\0';
 
     SETBIN (stdout);
 
-    while (get_word(stdin, cur)) {
+    while (get_word(stdin, cur, BUFSIZE)) {
       int i = 0;
       /* get the length of the prefix */
       while (prev[i] != '\0' && cur[i] != '\0' && prev[i] == cur[i])
-	++i;
+        ++i;
       if (i > 31) {
-	putc('\0', stdout);
+        putc('\0', stdout);
       }
       putc(i+1, stdout);
       fputs(cur+i, stdout);
       if (cur == s1) {
-	prev = s1; cur = s2;
+        prev = s1; cur = s2;
       } else {
-	prev = s2; cur = s1;
+        prev = s2; cur = s1;
       }
     }
     return 0;
@@ -98,16 +102,22 @@ int main (int argc, const char *argv[]) {
     i = getc(stdin);
     while (i != -1 ) {
       if (i == 0)
-	i = getc(stdin);
-      --i;  
-      while ((c = getc(stdin)) > 32)
-	cur[i++] = (char)c;
+        i = getc(stdin);
+      --i;
+      if (i < 0) goto error;
+      while ((c = getc(stdin)) > 32 && i < BUFSIZE)
+        cur[i++] = (char)c;
+      if (i >= BUFSIZE) goto error;
       cur[i] = '\0';
       fputs(cur, stdout);
       putc('\n', stdout);
       i = c;
     }
     return 0;
+
+   error:
+    fputs("ERROR: Corrupt Input.\n", stderr);
+    return 2;
 
   } else {
 
