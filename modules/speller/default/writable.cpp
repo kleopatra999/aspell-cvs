c2eaeb99923cbc3e62a18c5f65307e310bcf3cb0
Kevin Atkinson <kevina@gnu.org>
2011-07-04 10:30:26 +0000

    Release 0.60.6.1.
    Updated Copyright on some files, po file updates (due to change in
    copyright string).
    
    [Imported from GNU Aspell CVS Repository.]

--- modules/speller/default/writable.cpp
+++ modules/speller/default/writable.cpp
@@ -1,3 +1,9 @@
+// This file is part of The New Aspell
+// Copyright (C) 2000,2011 by Kevin Atkinson under the GNU LGPL
+// license version 2.0 or 2.1.  You should have received a copy of the
+// LGPL license along with this library if you did not you can find it
+// at http://www.gnu.org/.
+
 #include <time.h>
 
 #include "hash-t.hpp"
